ed7fa05cc0990c76a5d6956d66bf3a27a6eac77a
Ismael Slimane <isma.bled777@gmail.com>
2015-07-31 20:38:57 +0200

    Built the form to add new comments

--- Simple_Comments_App/commentBox.js
+++ Simple_Comments_App/commentBox.js
@@ -1,7 +1,16 @@
-var data = [
-  {author: "Pete Hunt", text: "This is one comment"},
-  {author: "Jordan Walke", text: "This is *another* comment"}
-];
+var Comment = React.createClass({
+	render: function(){
+		var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+		return(
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+			</div>
+		);
+	}
+});
 
 var CommentBox = React.createClass({
 	loadCommentsFromServer: function() {
@@ -17,6 +26,24 @@ var CommentBox = React.createClass({
 			}.bind(this)
 		});
 	},
+	handleCommentSubmit: function(comment) {
+		// TODO: submit to the server and refresh the list
+		var comments = this.state.data;
+	    var newComments = comments.concat([comment]);
+	    this.setState({data: newComments});
+	    $.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			type: 'POST',
+			data: comment,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+    	});
+ 	},
 	// "props" are immutable, to implement interactions we add
 	// "state".
 	getInitialState: function() {
@@ -27,12 +54,15 @@ var CommentBox = React.createClass({
 		this.loadCommentsFromServer();
     	setInterval(this.loadCommentsFromServer, this.props.pollInterval);
 	},
+	// We need to pass data from the child component back up to its
+	// parent. We do this in our parent's render method by passing a new
+	// callback (handleCommentSubmit) into the child
   	render: function() {
 	    return (
 	      <div className="commentBox">
 	        <h1>Comments</h1>
 	        <CommentList data={this.state.data} />
-	        <CommentForm />
+	        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
 	      </div>
 	    );
   	}
@@ -56,25 +86,34 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+	// Clear the form fields when the form is submitted with valid input
+	handleSubmit: function(e){
+		// Prevent the browser's default action of submitting the form
+		e.preventDefault();
+		// We use the "ref" attribute to assign a name to a child component
+		// and this.refs to reference the component
+		// We can call React.findDOMNode(component) on a component to get
+		// the native browser DOM element
+		var author = React.findDOMNode(this.refs.author).value.trim();
+		var text = React.findDOMNode(this.refs.text).value.trim();
+		if(!text || !author){
+			return;
+		}
+		// Let's call the callback from the CommentForm when the user submits
+		// the form
+		this.props.onCommentSubmit({author: author, text: text});
+		// TODO: send request to the server
+	    React.findDOMNode(this.refs.author).value = '';
+	    React.findDOMNode(this.refs.text).value = '';
+	    return;
+	},
 	render: function(){
 		return(
-			<div className="commentForm">
-				Hello, world! I am a CommentForm.
-			</div>
-		);
-	}
-});
-
-var Comment = React.createClass({
-	render: function(){
-		var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-		return(
-			<div className="comment">
-				<h2 className="commentAuthor">
-					{this.props.author}
-				</h2>
-				<span dangerouslySetInnerHTML={{__html: rawMarkup}} />
-			</div>
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+		        <input type="text" placeholder="Your name" ref="author" />
+		        <input type="text" placeholder="Say something..." ref="text" />
+				<input type="submit" value="Post" />
+			</form>
 		);
 	}
 });
